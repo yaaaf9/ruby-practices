8c8d03e34b6f855f48b486261d215f6f2143ac90
yaaaf9 <minicat3090tan@gmail.com>
2021-03-17 16:44:28 +0900

    29行目のif文を削除、およびブロック引数のアンダースコアをframeに修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,22 +22,21 @@ frames = shots.each_slice(2).to_a
 
 point = 0
 
-frames.each_with_index do |_, i|
+frames.each_with_index do |frame, i|
   point += \
     if i < 9
       # turkey or double strike
-      if frames[i][0] == 10 && frames[i + 1][0] == 10 && frames[i + 2][0] == 10 \
-      || frames[i][0] == 10 && frames[i + 1][0] == 10
+      if frame[0] == 10 && frames[i + 1][0] == 10
         10 + frames[i + 1][0] + frames[i + 2][0]
-      elsif frames[i][0] == 10  # strike
+      elsif frame[0] == 10  # strike
         10 + frames[i + 1][0] + frames[i + 1][1]
-      elsif frames[i].sum == 10 # spare
+      elsif frame.sum == 10 # spare
         10 + frames[i + 1][0]
       else
-        frames[i].sum
+        frame.sum
       end
     else # 10 frame
-      frames[i].sum
+      frame.sum
     end
 end
 
